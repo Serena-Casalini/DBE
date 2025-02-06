ed1893c14c8f5c8094018cc94b65e803bd8ad8cf
Serena-Casalini <146020272+Serena-Casalini@users.noreply.github.com>
2025-02-06 15:24:07 +0100

    Update DBE_scheme.py

--- DBE_scheme.py
+++ DBE_scheme.py
@@ -1,5 +1,9 @@
 from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, G2, GT, pair
 
+
+#! La differenza nasce dal modo in cui vengono “distribuite” le operazioni esponenziali nella libreria Charm
+# se sono uguali o no dipende da come e quando vengono effettuate le operazioni di esponenziazione e a come vengono memorizzati i risultati all'interno della libreria
+
 #############################################
 # Setup: genera i parametri pubblici
 #############################################
@@ -34,8 +38,15 @@ def KeyGen(pp, j):
     group, L, g1, g2, alpha, pp1, pp2 = pp
     t = group.random(ZR)
     # Chiave segreta: usk_j = [t * α^(L+1-j)]_2
-    usk = g2 ** (t * (alpha ** (L+1 - j)))
-    
+    usk = pp2[L+1-j]**t #! g2 ** (t * (alpha ** (L+1 - j)))
+    #i valori sono diversi ma il confronto da "usk é uguale"
+    '''print("usk", g2 ** (t * (alpha ** (L+1 - j))))
+    print("prova", pp2[L+1-j]**t)
+    if (g2 ** (t * (alpha ** (L+1 - j)))) == pp2[L+1-j]**t:
+        print("usk é uguale")
+    else:
+        print("usk NON corrisponde")'''
+
     # Chiave pubblica:
     #   - componente in G1: upk_first = [t]_1
     #   - componenti in G2: per ogni l = 1,...,L tranne l = L+1-j, 
@@ -45,8 +56,14 @@ def KeyGen(pp, j):
     for l in range(1, L+1):
         if l == (L+1 - j):
             continue  # saltiamo l'esponente "mancante"
-        upk_components[l] = g2 ** (t * (alpha ** l))
-    
+        upk_components[l] = pp2[l]**t #! g2 ** (t * (alpha ** l))
+        #i valori sono diversi ma il confronto da "usk é uguale"
+        '''print("upk_components", l,  g2 ** (t * (alpha ** l)))
+        print("prova1", pp2[l]**t)
+        if ( g2 ** (t * (alpha ** l))) == pp2[l]**t:
+            print("upk_component é uguale")
+        else:
+            print("upk NON corrisponde")'''
     # Restituiamo anche t (come scalare) per semplificare il calcolo in cifratura
     return (t, usk, (upk_first, upk_components))
 
@@ -72,13 +89,14 @@ def Enc(pp, public_keys, S, M):
     for j in S:
         t_j, _usk, _upk = public_keys[j]
         term1 = g1 ** (s * t_j) # [s*t_j]_1
-        term2 = pp1[j]      # ([α^j]_1)
+        term2 = pp1[j]       # ([α^j]_1)
         ct2 *= term1 * term2
         #print("term1", term1)
         #print("term2", term2)
     
     # ct3 = [s*α^(L+1)]_T * M, dove [s*α^(L+1)]_T = e( g1^(s*α), g2^(α^L) )
-    ct3_factor = group.pair_prod([g1 ** (s * alpha)], [g2 ** (alpha ** L)])
+    # g2 ** (alpha ** L) = pp2[L]
+    ct3_factor = group.pair_prod([g1 ** (s * alpha)], [pp2[L]])
     #print("ct3_factor", ct3_factor)
     #uguale a print("TEST", pair(g1 ** (s * alpha), g2 ** (alpha ** L), group.Pairing))
     ct3 = ct3_factor * M
@@ -100,10 +118,10 @@ def Dec(pp, public_keys, usk_i, ct, S, i):
     ct1, ct2, ct3 = ct
 
     # Calcola pp_{2L+1-i} = [-α^(L+1-i)]_2 = g2^(-α^(L+1-i))
-    pp_neg = g2 ** ( - (alpha ** (L+1 - i)) )
+    pp_neg = pp2[L+1-i] ** -1 #!g2 ** ( - (alpha ** (L+1 - i)) )
     #i valori sono diversi ma il confronto da "pp_neg é corretto"
-    '''print("pp_neg", pp_neg)
-    print("pp2[L+1-i]**-1", pp2[L+1-i]**-1)
+    #print("pp_neg", pp_neg)
+    '''print("pp2[L+1-i]**-1", pp2[L+1-i]**-1)
     if pp_neg == pp2[L+1-i] ** -1:
         print("pp_neg è corretto")
     else:
@@ -128,12 +146,13 @@ def Dec(pp, public_keys, usk_i, ct, S, i):
         # upk_j è una tupla: (upk_first, upk_components)
         comp = upk_j[1].get(L+1 - i, None)
         #print("upk_j",upk_j)
-        #print("COMP", comp)
+        print("COMP", comp)
         # uguale a print("VERIFICA", g2 ** ( t_j * ( alpha ** (L+1-i))) )
         if comp is None:
             raise Exception(f"Componente upk mancante per l'utente {j} per esponente {L+1-i}")
-        pp_component = g2 ** (alpha ** (L+1 + j - i)) 
-        #uguale a pp2[ L+1+j-i ]
+        pp_component = pp2[ L+1+j-i ] # g2 ** (alpha ** (L+1 + j - i)) 
+        print("pp_component", pp_component)
+        print("prova", g2 ** (alpha ** (L+1 + j - i)) )
  
         product *= comp * pp_component
     #print("product: ", product)
@@ -200,5 +219,3 @@ def test():
     #print("M_dec_norm:", group.serialize(M_dec_norm))
     assert M_norm == M_dec_norm, "Errore: La decifratura non è corretta!"
     print("[SUCCESS] Decifratura corretta.")
-
-test()
